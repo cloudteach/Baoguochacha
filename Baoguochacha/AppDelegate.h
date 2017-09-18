c1fd43997c1107ef8f9d1d78b1d6910aec1a53b1
lanzhao <lanzhao_bimworks@163.com>
2017-09-18 09:41:25 +0800

    project init

---
+++ Baoguochacha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baoguochacha
+//
+//  Created by tiny on 2017/9/18.
+//  Copyright © 2017年 tiny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baoguochacha/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Baoguochacha
+//
+//  Created by tiny on 2017/9/18.
+//  Copyright © 2017年 tiny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
